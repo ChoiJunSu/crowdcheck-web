6258fe26a739d98056b50fdd5f6a4b03cd5e2c3f
keroro10398 <keroro10398@gmail.com>
2022-02-07 00:57:50 +0900

    Deploying elasticbeanstalk

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000";
+export const API_URL = "http://Crowdcheckserverdev-env-1.eba-ybtnuj6m.ap-northeast-2.elasticbeanstalk.com";
 
-export const WEB_URL = "http://localhost:3000";
+export const WEB_URL = "http://Crowdcheckwebdev-env.eba-xnta3tdz.ap-northeast-2.elasticbeanstalk.com";
