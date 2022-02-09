5619160d274d990b1beb8853e305ec414ea1cb04
ChoiJunSu <keroro10398@gmail.com>
2022-02-09 11:36:54 +0900

    Update url

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://Crowdcheckserverdev-env-1.eba-ybtnuj6m.ap-northeast-2.elasticbeanstalk.com";
+export const API_URL = "http://server.dev.crowdcheck.io";
 
-export const WEB_URL = "http://Crowdcheckwebdev-env.eba-xnta3tdz.ap-northeast-2.elasticbeanstalk.com";
+export const WEB_URL = "http://web.dev.crowdcheck.io";
